e1484fbcd809eccb3bdaec6ccc100c6e7e7409ef
stein <steicb18@wfu.edu>
2019-09-05 16:28:14 -0400

    Chris

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
     int revVctr[8];               // User values
+    int revVctr1[8];
     int i;                        // Loop index
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
@@ -13,13 +14,13 @@ int main() {
 
     // Reverse
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+        revVctr1[i] = revVctr[NUM_ELEMENTS - 1 - i]; // Swap
     }
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+        cout << " " << revVctr1[i];
     }
     cout << endl;
 
